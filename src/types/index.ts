f5c5b362c68b2f7bc260817640bb19f34c8261e0
Thyago <laurapausini8477@gmail.com>
2026-01-09 19:53:15 -0300

    refactor: organiza tipos de UI e interface em arquivos dedicados (#7)

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+// [09/01 19:56] Centralizador de exportação de tipos para o sistema
+/**
+ * Ponto central de exportação de tipos.
+ * Agrupa definições de UI, Fechamento e Database.
+ */
+export * from './ui';
+export * from './fechamento';
+export * from './database';
